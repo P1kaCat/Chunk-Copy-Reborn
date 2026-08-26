08bacc5e90268c0fff5f86804e65e74d105e2d00
PikaCat <pikacat.contact@gmail.com>
2026-08-26 13:21:11 +0200

    Port client command registration to Fabric API 26.2

--- src/main/java/thecsdev/chunkcopy/client/ChunkCopyClient.java
+++ src/main/java/thecsdev/chunkcopy/client/ChunkCopyClient.java
@@ -3,35 +3,22 @@ package thecsdev.chunkcopy.client;
 import org.jetbrains.annotations.Nullable;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import thecsdev.chunkcopy.ChunkCopy;
 import thecsdev.chunkcopy.client.command.ChunkCopyClientCommand;
 import thecsdev.chunkcopy.command.ChunkCopyCommand;
 
-/**
- * The {@link ChunkCopy} initializer for clients.
- */
 public final class ChunkCopyClient extends ChunkCopy implements ClientModInitializer
 {
-	// ==================================================
-	/**
-	 * The client-side version of the {@link ChunkCopyCommand}.
-	 */
 	private ChunkCopyClientCommand Command = null;
-	// ==================================================
-	@Override
-	public void onInitializeClient()
+	@Override public void onInitializeClient()
 	{
-		//register the command
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, buildContext) ->
 		{
 			Command = new ChunkCopyClientCommand();
-			Command.register(ClientCommandManager.getActiveDispatcher(), registryAccess);
+			Command.register(dispatcher, buildContext);
 		});
 	}
-	// --------------------------------------------------
-	@Override
-	public @Nullable ChunkCopyCommand<?> getCommand() { return Command; }
-	// ==================================================
+	@Override public @Nullable ChunkCopyCommand<?> getCommand() { return Command; }
 }
