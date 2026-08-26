fe38367a9ad1bd66749a26afe88b2d209378a559
PikaCat <pikacat.contact@gmail.com>
2026-08-26 13:21:16 +0200

    Port chunk load callback and ChunkPos access to 26.2

--- src/main/java/thecsdev/chunkcopy/ChunkCopy.java
+++ src/main/java/thecsdev/chunkcopy/ChunkCopy.java
@@ -24,14 +24,13 @@ public abstract class ChunkCopy
 {
 	private static ChunkCopy Instance = null;
 	public static final Logger LOGGER = LoggerFactory.getLogger(getModID());
-	public static final String ModName     = "Chunk Copy";
-	public static final String ModID       = "chunkcopy";
-	public static final int    FileVersion = 759;
+	public static final String ModName = "Chunk Copy";
+	public static final String ModID = "chunkcopy";
+	public static final int FileVersion = 759;
 
 	public ChunkCopy()
 	{
-		if(validateInstance())
-			throw new RuntimeException("An instance of ChunkCopy already exists.");
+		if(validateInstance()) throw new RuntimeException("An instance of ChunkCopy already exists.");
 		Instance = this;
 		LOGGER.info("Initializing '" + getModName() + "' as '" + getClass().getSimpleName() + "'.");
 		ChunkCopyConfig.KEYS.add(ChunkCopyConfig.PASTE_ENTITIES);
@@ -39,51 +38,27 @@ public abstract class ChunkCopy
 		ChunkData.registerChunkDataBlockType(CDBEntityBlocksLegacy.class);
 		ChunkData.registerChunkDataBlockType(CDBEntitiesLegacy.class);
 
-		ServerChunkEvents.CHUNK_LOAD.register((sWorld, sChunk) ->
+		ServerChunkEvents.CHUNK_LOAD.register((sWorld, sChunk, generated) ->
 		{
 			if(!AutoChunkCopy.isPasting()) return;
 			final ChunkPos scPos = sChunk.getPos();
-			final Runnable task = () ->
-			{
-				try
-				{
-					final String fileName = AutoChunkCopy.getFileName();
-					ChunkCopyAPI.loadChunkDataIO(sWorld, sChunk.getPos(), fileName);
-				}
-				catch(Exception exc) {}
-			};
-			new Thread(() ->
-			{
-				try
-				{
-					Thread.sleep(500);
-					while(!sWorld.hasChunk(scPos.x, scPos.z))
-						Thread.sleep(100);
-					task.run();
-				}
-				catch (Exception e) {}
-			}).start();
+			final Runnable task = () -> { try { ChunkCopyAPI.loadChunkDataIO(sWorld, scPos, AutoChunkCopy.getFileName()); } catch(Exception exc) {} };
+			new Thread(() -> { try { Thread.sleep(500); while(!sWorld.hasChunk(scPos.getX(), scPos.getZ())) Thread.sleep(100); task.run(); } catch(Exception e) {} }).start();
 		});
 	}
 
 	public static String getModName() { return ModName; }
 	public static String getModID() { return ModID; }
 	@Nullable public static ChunkCopy getInstance() { return Instance; }
-	public static boolean validateInstance()
-	{
-		if(Instance != null && (Instance instanceof ChunkCopyClient || Instance instanceof ChunkCopyServer))
-			return true;
-		else return false;
-	}
+	public static boolean validateInstance() { return Instance != null && (Instance instanceof ChunkCopyClient || Instance instanceof ChunkCopyServer); }
 	public static EnvType getEnviroment()
 	{
 		if(!validateInstance()) throw new RuntimeException("Uninitialized mod.");
 		if(Instance instanceof ChunkCopyClient) return EnvType.CLIENT;
-		else if(Instance instanceof ChunkCopyServer) return EnvType.SERVER;
-		else throw new RuntimeException("If you are reading this, something went terribly wrong.");
+		if(Instance instanceof ChunkCopyServer) return EnvType.SERVER;
+		throw new RuntimeException("If you are reading this, something went terribly wrong.");
 	}
-	public static File getModSavesDirectory()
-	{ return new File(getRunDirectory().getAbsolutePath() + "/mods/" + ModID + "/"); }
+	public static File getModSavesDirectory() { return new File(getRunDirectory().getAbsolutePath() + "/mods/" + ModID + "/"); }
 	public static File getRunDirectory() { return new File(System.getProperty("user.dir")); }
 	public static boolean isServer() { return getEnviroment() == EnvType.SERVER; }
 	public static boolean isClient() { return getEnviroment() == EnvType.CLIENT; }
